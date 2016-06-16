cd2717992a238e457a9d440fd5e6dfe897b1543d
SamanthaJones <Github@123>
2016-06-16 14:19:21 +0530

    Modification done

--- IPFinder.java
+++ IPFinder.java
@@ -10,11 +10,12 @@ public static void main(String[] args) {
         InetAddress ia=InetAddress.getByName(new URL(url).getHost());  
         String ip=ia.getHostAddress();  
       System.out.println("The IP is:" +ip);  
-    } catch (UnknownHostException e1) {  
-        System.out.println("Exception is :"+e1.toString());  
+    } catch (UnknownHostException e1)
+	 {  
+        System.out.println("The Exception is :"+e1.toString());  
     }   
 	catch (Exception e2) {  
-        System.out.println("Exception is :"+e2.toString());  
+        System.out.println("The Exception is :"+e2.toString());  
     }  
 }  
 }
